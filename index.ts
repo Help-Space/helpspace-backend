5e83b8f9e16c01dbcaf97695a370635341cfd995
Artiu <71273256+Artiu@users.noreply.github.com>
2022-10-11 00:57:23 +0200

    Add dist dir to gitignore

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { connect } from "mongoose";
 
 dotenv.config();
+
+connect("mongodb://localhost:27017");
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
