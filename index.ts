2494f9e3c6501f08e08a7af0c29d129c5912220f
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-10-11 11:59:55 +0200

    Create express router for user

--- index.ts
+++ index.ts
@@ -1,24 +1,28 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import { connect } from "mongoose";
+import user from "./routes/user";
 
 dotenv.config();
 
+const app: Express = express();
+const port = process.env.PORT || 3001;
 const mongodbUrl = process.env.MONGODB_URI;
+
 if(!mongodbUrl) {
     throw new Error("Mongo URL is not provided");
 }
 
+app.use(express.json());
+
 connect(mongodbUrl).catch((err) => {
-    throw err
+    console.error(err);
+    app.all("*", (req, res) => {
+        res.status(500).send("Something went wrong");
+    });
 })
 
-const app: Express = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
-});
+app.use("/user", user);
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ routes/user.ts
@@ -0,0 +1,45 @@
+import {Router} from "express";
+import {body, validationResult} from "express-validator";
+import User from "../models/user";
+
+const router = Router();
+
+router.post("/register",
+    body("first_name")
+        .isString()
+        .isLength({ min: 2, max: 20 })
+        .trim(),
+    body("last_name")
+        .isString()
+        .isLength({ min: 2, max: 30 })
+        .trim(),
+    body("email")
+        .isEmail()
+        .normalizeEmail()
+        .custom(async (value) => {
+            const user = await User.findOne({ email: value });
+            if (user) {
+                return Promise.reject("Email already in use");
+            }
+        }),
+    body("password")
+        .isStrongPassword({minLength: 6, minLowercase: 2, minUppercase: 2, minNumbers: 1})
+        .isLength({ min: 6, max: 128 }),
+    body("birth_date")
+        .isISO8601(),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        createUser(req.body).then((ok: boolean) => {
+
+        }, (err: Error) => {
+            res.status(500).send(err);
+        })
+
+        res.send("Hello World");
+});
+
+export default router;
