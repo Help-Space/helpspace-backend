82f2037f9114fde6767aaf6f8dc04f1bc6e9f98d
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-10-16 03:56:00 +0200

    Add UserValidator

--- routes/user.js
+++ routes/user.js
@@ -9,21 +9,11 @@ const router = Router();
 
 router.post(
     "/register",
-    body("first_name").isString().isLength({ min: 2, max: 20 }).trim(),
-    body("last_name").isString().isLength({ min: 2, max: 30 }).trim(),
-    body("email")
-        .isEmail()
-        .normalizeEmail()
-        .custom(async (value) => {
-            const user = await User.findOne({ email: value });
-            if (user) {
-                return Promise.reject("Email already in use");
-            }
-        }),
-    body("password")
-        .isStrongPassword({ minLength: 6, minLowercase: 2, minUppercase: 2, minNumbers: 1 })
-        .isLength({ min: 6, max: 128 }),
-    body("birth_date").isISO8601(),
+    UserValidator.checkFirstName(),
+    UserValidator.checkLastName(),
+    UserValidator.checkEmail(),
+    UserValidator.checkPassword(),
+    UserValidator.checkBirthDate(),
     handleValidator,
     async (req, res) => {
         const { first_name, last_name, email, password, birth_date } = req.body;
---
+++ validators/user.js
@@ -0,0 +1,44 @@
+import { body } from "express-validator";
+import User from "../models/user.js";
+
+export class UserValidator {
+    static checkFirstName() {
+        return body("first_name").isString().isLength({ min: 2, max: 20 }).trim();
+    }
+
+    static checkLastName() {
+        return body("last_name").isString().isLength({ min: 2, max: 30 }).trim();
+    }
+
+    static checkEmail(isRegister = true) {
+        return body("email")
+            .isEmail()
+            .normalizeEmail()
+            .custom(async (value) => {
+                const user = await User.findOne({ email: value });
+                if (isRegister) {
+                    if (user) {
+                        return Promise.reject("Email already in use");
+                    }
+                } else {
+                    if (!user) {
+                        return Promise.reject("User not found");
+                    }
+                }
+            });
+    }
+
+    static checkPassword(isRegister = true) {
+        if (isRegister) {
+            return body("password")
+                .isStrongPassword({ minLength: 6, minLowercase: 2, minUppercase: 2, minNumbers: 1 })
+                .isLength({ min: 6, max: 128 });
+        } else {
+            return body("password").notEmpty();
+        }
+    }
+
+    static checkBirthDate() {
+        return body("birth_date").isISO8601();
+    }
+}
