1aebb6a9b795400a61f62c626e39f9cc3d103a76
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-10-21 20:21:27 +0200

    Add error handling in messageListener, fix condition

--- listeners/messageListener.js
+++ listeners/messageListener.js
@@ -7,11 +7,17 @@ const messageListener = async (socket, data) => {
     const userId = socket.user.id;
     const { conversationId, message } = data;
     if (!mongoose.Types.ObjectId.isValid(conversationId)) {
+        console.error("messageListener -> Invalid conversation id", conversationId);
         return;
     }
     const select = "first_name last_name _id";
     const conversation = await Conversation.findById(conversationId).populate("post_owner", select).populate("user", select).exec();
-    if (!conversation || (conversation.post_owner.toString() !== userId && conversation.user.toString() !== userId)) {
+    if (!conversation) {
+        console.error("messageListener -> Conversation not found", conversationId);
+        return;
+    }
+    if ((conversation.post_owner._id.toString() !== userId) && (conversation.user._id.toString() !== userId)) {
+        console.error("messageListener -> Insufficient permissions", conversationId);
         return;
     }
     const newMessage = new Message({
